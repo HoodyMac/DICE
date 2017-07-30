6a93f1d6ca5b679c5785a7d4be50702bd34ba660
Maksym Osmuk <maxim.osmuk@gmail.com>
2017-07-30 13:35:01 +0200

    Security entities

---
+++ dice-api/src/main/java/pl/zed/dice/security/domain/AuthorityName.java
@@ -0,0 +1,5 @@
+package pl.zed.dice.security.domain;
+
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ dice-api/src/main/java/pl/zed/dice/security/domain/User.java
@@ -0,0 +1,86 @@
+package pl.zed.dice.security.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    @Size(min = 4, max = 50)
+    private String username;
+
+    @NotNull
+    @Size(min = 4, max = 100)
+    private String password;
+
+    @NotNull
+    private Boolean enabled;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @NotNull
+    private Date lastPasswordResetDate;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_id", referencedColumnName = "id")}
+    )
+    private List<Authority> authorities;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Date getLastPasswordResetDate() {
+        return lastPasswordResetDate;
+    }
+
+    public void setLastPasswordResetDate(Date lastPasswordResetDate) {
+        this.lastPasswordResetDate = lastPasswordResetDate;
+    }
+
+    public List<Authority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
+    }
+}
