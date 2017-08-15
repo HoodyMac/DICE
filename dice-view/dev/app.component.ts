6c414d8ed533f71f74aa681eaf1b9759919a89cf
Maksym Osmuk <maxim.osmuk@gmail.com>
2017-08-15 21:25:58 +0200

    Added bootstrap

--- dice-view/dev/app.component.ts
+++ dice-view/dev/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'my-app',
+    selector: 'dice-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World!</p>
