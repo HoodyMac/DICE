d1b79cd8887d27d46b4ec49d2df6577a7c04e36b
Maksym Osmuk <maxim.osmuk@gmail.com>
2017-08-18 19:00:30 +0200

    Added navbar component Added profile page component Added routing for existing components

--- dice-view/dev/app.component.ts
+++ dice-view/dev/app.component.ts
@@ -1,12 +1,21 @@
 import {Component} from 'angular2/core';
+import {HomeComponent} from "./home/home.component";
+import {ROUTER_DIRECTIVES, RouteConfig} from "angular2/router";
+import {ProfileComponent} from "./profile/profile.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
     selector: 'dice-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <dice-nav></dice-nav>
+        <router-outlet></router-outlet>
     `,
+    directives: [ROUTER_DIRECTIVES, NavbarComponent, HomeComponent, ProfileComponent]
 })
+@RouteConfig([
+  { path: '/home', name: 'Home', component: HomeComponent },
+  { path: '/profile', name: 'Profile', component: ProfileComponent }
+])
 export class AppComponent {
 
 }
---
+++ dice-view/dev/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'dice-nav',
+  templateUrl: 'dev/navbar/navbar.component.html'
+})
+export class NavbarComponent {
+
+}
+
