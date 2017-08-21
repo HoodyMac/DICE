a68a08ac3069450edfc43364e18202d54e77cacc
Maksym Osmuk <maxim.osmuk@gmail.com>
2017-08-21 19:09:26 +0200

    Consume auth service in HomeComponent

--- dice-view/dev/home/home.component.ts
+++ dice-view/dev/home/home.component.ts
@@ -1,10 +1,27 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {AuthenticationService} from "../authentication/authentication.service";
+import {ControlGroup, FormBuilder, Validators} from "angular2/common";
 
 @Component({
   templateUrl: 'dev/home/home.component.html',
   styleUrls: ['../app/css/home.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public singInForm: ControlGroup;
 
+  constructor(
+    private _authenticationService: AuthenticationService,
+    private _formBuilder: FormBuilder) {}
+
+  ngOnInit(): any {
+    this.singInForm = this._formBuilder.group({
+      'username': ['', Validators.required],
+      'password': ['', Validators.required],
+    });
+  }
+
+  onSignIn(credentials) {
+    this._authenticationService.doLogin(credentials);
+  }
 }
 
