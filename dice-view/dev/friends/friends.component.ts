ca63a481aaf45595e62332f849770224e95010e7
Ivan_Loichuk <ivan.loichuk@pollub.edu.pl>
2017-08-28 22:15:22 +0200

    add friends page  (html and style)

---
+++ dice-view/dev/friends/friends.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: 'dev/friends/friends.component.html',
+  styleUrls: ['../app/css/friends.css']
+})
+export class FriendsComponent {
+
+}
+
+
