e63ccc505046a857bb5972686de2afc4d5f18770
Maksym Osmuk <maxim.osmuk@gmail.com>
2017-10-12 20:29:20 +0200

    Fix navbar service

--- dice-view/dev/app.component.ts
+++ dice-view/dev/app.component.ts
@@ -1,8 +1,11 @@
 import {Component} from "@angular/core";
+import {AuthenticationService} from "./common/services/authentication.service";
 
 @Component({
     selector: 'dice-app',
     templateUrl: 'dev/app.component.html',
 })
 export class AppComponent {
+
+  constructor(public authenticationService: AuthenticationService) {}
 }
