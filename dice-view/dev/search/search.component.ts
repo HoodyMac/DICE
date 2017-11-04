5e1b9f68e6c736b6a7362d3c134df1a367ce599a
Ivan_Loichuk <ivan.loichuk@pollub.edu.pl>
2017-11-04 23:34:59 +0100

    added service and FE for 'search page'

--- dice-view/dev/search/search.component.ts
+++ dice-view/dev/search/search.component.ts
@@ -1,40 +1,86 @@
 import {Component, AfterViewInit, ElementRef, ViewChild} from '@angular/core';
+import {SearchService} from '../services/search.service'
+import {FormGroup, FormBuilder, NgForm} from "@angular/forms";
 declare var jQuery: any;
 
 @Component({
     templateUrl: 'dev/search/search.component.html',
-    styleUrls: ['../app/css/search.css']
+    styleUrls: ['../app/css/search.css'],
+    providers: [SearchService, NgForm]
 })
+
 export class SearchComponent implements AfterViewInit{
     @ViewChild('choseLang') choseLang: ElementRef;
     @ViewChild('choseAgeFrom') choseAgeFrom: ElementRef;
     @ViewChild('choseAgeTo') choseAgeTo: ElementRef;
     ageValues: any = new Array();
-    constructor() {
-        //generate age array
+
+    searchData = [];
+    public searchForm: FormGroup;
+    public sForm: NgForm;
+
+    constructor(private searchService: SearchService,
+                private fb:FormBuilder,
+                private sForm: NgForm) {
+        //generate age value
         for (let i = 1; i <= 100; i++) {
             this.ageValues.push(i);
         }
+
+        this.searchForm = fb.group({
+            city: '',
+            ageFrom: '',
+            ageTo: '',
+            gender: 'NULL',
+            isOnline: '',
+            programmingLanguages: '',
+            fullName: ''
+        });
+
     }
 
     ngAfterViewInit() {
+        let mainClass = this;
+        let searchForm = this.searchForm;
         jQuery(this.choseLang.nativeElement).chosen();
         jQuery(this.choseAgeFrom.nativeElement).chosen();
         jQuery(this.choseAgeTo.nativeElement).chosen();
+
         jQuery('.chosen-container-single').css('width', '70px');
         jQuery('#lang_select > .chosen-container-single').css('width', '90%');
         jQuery('#lang_select > .chosen-container-single').css('margin', '5px 5%');
 
 
         jQuery(this.choseAgeFrom.nativeElement).chosen().change(function () {
-            jQuery(this.choosenSelect).val();  //your value
+            mainClass.getUsersData(searchForm.value);
         });
         jQuery(this.choseAgeTo.nativeElement).chosen().change(function () {
-            jQuery(this.choosenSelect).val();  //your value
+            mainClass.getUsersData(searchForm.value);
         });
         jQuery(this.choseLang.nativeElement).chosen().change(function () {
-            jQuery(this.choosenSelect).val();  //your value
+            mainClass.getUsersData(searchForm.value);
         });
+
+    }
+
+    getUsersData(searchData){
+        searchData['ageFrom'] = jQuery(this.choseAgeFrom.nativeElement).val();
+        searchData['ageTo'] = jQuery(this.choseAgeTo.nativeElement).val();
+        searchData['programmingLanguages'] = jQuery(this.choseLang.nativeElement).val();
+        searchData['gender'] =  this.searchForm.controls['gender'].value;
+        searchData['city'] = this.searchForm.controls['city'].value;
+        searchData['fullName'] = this.searchForm.controls['fullName'].value;
+        searchData['gender'] = this.searchForm.controls['gender'].value;
+        console.log(this.searchForm.controls['gender'].value);
+        console.log(searchData);
+
+        this.searchService.getUserSearchData(searchData)
+            .subscribe(
+                data =>{
+                    this.searchData = data;
+                    console.log(this.searchData);
+                }
+            );
+
     }
 }
-
---
+++ dice-view/dev/services/search.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import  'rxjs/add/operator/map';
+import {HttpClient} from "../common/services/http-client.service";
+
+@Injectable()
+export class SearchService {
+
+    constructor(private http: HttpClient) {
+        console.log('PostService Initialized...');
+    }
+
+    getUserSearchData(userSearchData){
+
+        return this.http.post("/api/search", userSearchData).map(res => res.json());
+    }
+}
