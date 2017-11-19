c72e1db4d4cafa55ede44f0a64d8e75dd492cb37
Roman Mysan <romanveos@gmail.com>
2017-11-19 15:21:20 +0200

    before merge

--- dice-view/dev/profile/profile.component.ts
+++ dice-view/dev/profile/profile.component.ts
@@ -3,8 +3,6 @@ import {ProfileService} from "../services/profile.service";
 import {Router, ActivatedRoute } from '@angular/router';
 import {SearchService} from '../services/search.service';
 import {FriendsService} from '../services/friends.service';
-import {TranslateService} from "ng2-translate";
-import {Title} from "@angular/platform-browser";
 
 let clicked = true;
 declare var jQuery: any;
@@ -32,10 +30,7 @@ export class ProfileComponent implements AfterViewInit{
   constructor(
     private profileService: ProfileService,
     private _router: Router, private route: ActivatedRoute, private _searchService: SearchService,
-    private _friendService: FriendsService,
-    private titleService: Title,
-    private translate: TranslateService) {
-
+    private _friendService: FriendsService) {
 
     this.route.params.subscribe(params => {
       this.profileId = params['id'];
@@ -49,14 +44,20 @@ export class ProfileComponent implements AfterViewInit{
           if(this.jcropApi !== undefined) {
             this.jcropApi.setImage('/api/profile/image/get/' + this.userInfo.originalImgSrc);
           }
-          translate.get('PAGE_TITLES.PROFILE', {username: this.userInfo.firstname + " " + this.userInfo.lastname } ).subscribe((res: string) => {
-            this.titleService.setTitle(res);
-          });
         },
         err => {
           console.log('Something went wrong!');
         }
     );
+
+    // this.profileService.getModule("server_url").subscribe(module => {
+    //       this.countModules = module;
+    //     },
+    //     err => {
+    //       console.log('Something went wrong!');
+    //     }
+    // );
+
   };
 
   goToMessagePage(){
@@ -115,6 +116,7 @@ export class ProfileComponent implements AfterViewInit{
          setTimeout(function() {
            this.viewImageChangeMessage = false;
          }.bind(this), 6000);
+         console.log(data);
        }
      );
   };
